708c036f94ee1725c59b7b9e8305491be6230861
Teddy Mwai <teddymwai11@gmail.com>
2022-05-25 01:28:59 +0300

    cleaned up project files and adding states

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// Adding components
+import {useState} from 'react';
+import Player from "./components/Player";
+import Song from "./components/Song";
+import data from "./util";
+import "./styles/app.scss";
 
 function App() {
+  // State
+  const [songs, setSongs] = useState(data());
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Song />
+      <Player />
     </div>
   );
 }
