56bf9d5f209c24ebaa2971538b9f21bac438e632
Teddy Mwai <teddymwai11@gmail.com>
2022-05-25 02:33:10 +0300

    added slider functionality and removed padding

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // Adding components
-import {useState} from 'react';
+import { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import data from "./util";
@@ -8,10 +8,16 @@ import "./styles/app.scss";
 function App() {
   // State
   const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
